729eeb7d2b6fda853a1e8b761b71183f1095f8fb
tom bertrand <tom.bertrand@mindgeek.com>
2016-01-12 18:29:03 -0500

    Version 2.3.2
    Feature
    - #139 Added ``resultCountPerGroup`` parameter to ``onResult`` callback
    
    Fixes
    - #134 Fixed XSS vulnerability
    - #136 Fixed UMD module definition
    
    Note
    - ``onSearch`` callback now gets triggered when a key is pressed (not only when query > options.minLength)

--- src/jquery.typeahead.js
+++ src/jquery.typeahead.js
@@ -4,7 +4,7 @@
  * Licensed under the MIT license
  *
  * @author Tom Bertrand
- * @version 2.3.1 (2016-01-12)
+ * @version 2.3.2 (2016-01-12)
  * @link http://www.runningcoder.org/jquerytypeahead/
 */
 ;
@@ -37,7 +37,7 @@
 }(function ($) {
 
     window.Typeahead = {
-        version: '2.3.1'
+        version: '2.3.2'
     };
 
     "use strict";
