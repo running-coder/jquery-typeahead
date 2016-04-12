512db1116c58b6d8f1ff75d1396813c81bfcd819
tom bertrand <tom.bertrand@mindgeek.com>
2016-04-12 17:08:25 -0400

    Version 2.5.1
    Fix
    - hint's input CSS was broken in 2.5.0 version
    
    Note
    - Pressing "ESC" when the Typeahead input is focus and empty should call `hideLayout()` as well of `blur`
    - Added `aria-hidden="true"` on hint's input for screen readers

--- src/jquery.typeahead.js
+++ src/jquery.typeahead.js
@@ -4,7 +4,7 @@
  * Licensed under the MIT license
  *
  * @author Tom Bertrand
- * @version 2.5.0 (2016-4-12)
+ * @version 2.5.1 (2016-4-12)
  * @link http://www.runningcoder.org/jquerytypeahead/
  */;
 (function (factory) {
@@ -32,7 +32,7 @@
     "use strict";
 
     window.Typeahead = {
-        version: '2.5.0'
+        version: '2.5.1'
     };
 
     /**
