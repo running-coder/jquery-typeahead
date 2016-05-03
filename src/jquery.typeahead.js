b0869002f4c202719f54b165ba211b3cdfe89608
tom bertrand <time2k1ll@hotmail.com>
2016-05-02 21:38:41 -0400

    Version 2.6.1 Fixes - #189 Twice url encoding - #190 Strange JS-code fragment in href attribute value in result list when using jQuery version below 1.10 - #192 Results remain open when clicking inside a second typeahead field - #193 Clicking on a suggested option does not select it on IE11

--- src/jquery.typeahead.js
+++ src/jquery.typeahead.js
@@ -4,7 +4,7 @@
  * Licensed under the MIT license
  *
  * @author Tom Bertrand
- * @version 2.6.0 (2016-5-2)
+ * @version 2.6.1 (2016-5-2)
  * @link http://www.runningcoder.org/jquerytypeahead/
  */;
 (function (factory) {
@@ -32,7 +32,7 @@
     "use strict";
 
     window.Typeahead = {
-        version: '2.6.0'
+        version: '2.6.1'
     };
 
     /**
