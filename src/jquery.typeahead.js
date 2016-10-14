e9bfc301f50f1efecae6e8fdbce8df97f3836be0
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-10-14 18:55:56 +0200

    Pass original event to the click handler

--- src/jquery.typeahead.js
+++ src/jquery.typeahead.js
@@ -1335,7 +1335,7 @@
                 if (activeItem.length > 0) {
                     // Prevent form submit if an element is selected
                     e.preventDefault();
-                    activeItem.find('a:first')[0].click();
+                    activeItem.find('a:first')[0].click(e, function () {});
                 }
                 return;
             }
