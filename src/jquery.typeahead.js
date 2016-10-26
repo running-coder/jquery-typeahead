ae902dda2b49872e9eca3a59a8d0a79dfb96e91c
Tom Bertrand <bertrand.tom@hotmail.com>
2016-10-26 16:58:35 -0400

    Version 2.7.2
    Fix #243 On form reset, hide the result list
    Fix #248 Dynamic Ajax GET query would not generate dropdown list when aborted
    Fix #249 Fix the Click action when `options.href` is defined

--- src/jquery.typeahead.js
+++ src/jquery.typeahead.js
@@ -4,7 +4,7 @@
  * Licensed under the MIT license
  *
  * @author Tom Bertrand
- * @version 2.7.1 (2016-10-25)
+ * @version 2.7.2 (2016-10-26)
  * @link http://www.runningcoder.org/jquerytypeahead/
  */
 ;(function (factory) {
@@ -32,7 +32,7 @@
     "use strict";
 
     window.Typeahead = {
-        version: '2.7.1'
+        version: '2.7.2'
     };
 
     /**
