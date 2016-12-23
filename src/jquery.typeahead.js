910abc164289a693b0f73609b1faa81b798d3d34
Tom Bertrand <bertrand.tom@hotmail.com>
2016-12-23 10:35:20 -0500

    Version 2.7.6
    Fix #271 Data is cached inside the xhrObject

--- src/jquery.typeahead.js
+++ src/jquery.typeahead.js
@@ -4,7 +4,7 @@
  * Licensed under the MIT license
  *
  * @author Tom Bertrand
- * @version 2.7.5 (2016-12-22)
+ * @version 2.7.6 (2016-12-23)
  * @link http://www.runningcoder.org/jquerytypeahead/
  */
 ;(function (factory) {
@@ -32,7 +32,7 @@
     "use strict";
 
     window.Typeahead = {
-        version: '2.7.5'
+        version: '2.7.6'
     };
 
     /**
